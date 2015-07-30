1bb7c4cedb8d5398c2b0116d7415b88ec0792e5b
nmchlcstll <i.michaelcastillo@gmail.com>
2015-07-30 15:16:11 -0500

    Added Swig, fixed logo

--- app.js
+++ app.js
@@ -1,9 +1,59 @@
 var http = require('http');
 var express = require('express');
+var swig = require('swig');
 var app = express();
 
 // Setup static file serving
 app.use(express.static('public'));
+// Adding Swig as a Templating Engine
+app.engine('swig', swig.renderFile);
+app.set('view engine', 'swig');
+
+function userTemplate(user) {
+    return [
+      '<h1>' + user.name + '</h1> (@' + user.username + ')<br />',
+      '<img src="' + user.photo_url + '" />'
+      ].join("\n");
+}
+app.get('/users/1', function (req, res) {
+  var user = {
+    id: 1,
+    username: 'iancastillo',
+    name: 'Ian Castillo',
+    photo_url: 'https://avatars2.githubusercontent.com/u/4645700?v=3&s=400'
+  };
+
+  res.send(userTemplate(user));
+});
+
+function charityTemplate(charity){
+    return[
+        '<h1>' + charity.charityname + '</h1> <br/>',
+        '<img src="' + charity.photo_url + '" />'
+        ].join("\n");
+}
+app.get('/charity/1', function (req, res){
+ var user = {
+    id: 1,
+    charityname: 'codeup',
+    name: 'Ian Castillo',
+    photo_url: 'https://avatars2.githubusercontent.com/u/4645700?v=3&s=400'
+  };
+  var user2 = {
+    id: 2,
+    charityname: 'codeup',
+    name: 'Steve Davis',
+    photo_url: 'https://avatars2.githubusercontent.com/u/4645700?v=3&s=400'
+  };
+
+  res.send(charityTemplate(user) + charityTemplate(user2) + charityTemplate({}));
+});
+
+
+app.get('/', function (req, res){
+  res.render('index', {});
+});
+
 
 // Listen on port 8000, IP defaults to 127.0.0.1
 server = app.listen(3000);
--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,14 @@ function buttonclick()
 $("body").append('<div class="dialog">hello!!</div>');
 }
 $("hello").remove('')
+
+function progressBarTemplate(percent) {
+    return '<div class="progress-bar" role="progressbar" aria-valuenow="' + percent + '" aria-valuemin="10" aria-valuemax="100"  style="min-width: ' + percent + '%;"> '+ percent + '%</div>'
+}
+
+$(function () {
+    $('.progress').each(function (index, element) {
+        var renderedProgressBar = progressBarTemplate($(element).data('percent'));
+        $(element).html(renderedProgressBar);
+    });
+})
