e6d8b94df0d296dc0275b2afa33995e8f61e8646
nmchlcstll <i.michaelcastillo@gmail.com>
2015-08-06 16:52:19 -0500

    Big Commit
    Progress bar now shows text percentage, menu bar now shows less menu
    items, tables better responsive

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,12 @@
 
 function buttonclick()
 {
-$("body").append('<div class="dialog">hello!!</div>');
+    $("body").append('<div class="dialog">hello!!</div>');
 }
-$("hello").remove('')
 
 function progressBarTemplate(percent) {
-    return '<div class="progress-bar" role="progressbar" aria-valuenow="' + percent + '" aria-valuemin="10" aria-valuemax="100"  style="min-width: ' + percent + '%;"> '+ percent + '%</div>'
+    return '<div class="progress-bar" role="progressbar" aria-valuenow="' + percent +
+     '" aria-valuemin="10" aria-valuemax="100"  style="min-width: ' + percent + '%;"> '+ percent + '%</div>';
 }
 
 $(function () {
@@ -15,4 +15,4 @@ $(function () {
         var renderedProgressBar = progressBarTemplate($(element).data('percent'));
         $(element).html(renderedProgressBar);
     });
-})
+});
