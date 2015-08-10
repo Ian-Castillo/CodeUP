95ad298e999f5f4ab595cbb0966ddebbefa0bbb4
nmchlcstll <i.michaelcastillo@gmail.com>
2015-08-10 16:55:01 -0500

    Added Database

---
+++ Database/bootstrap.js
@@ -0,0 +1,20 @@
+var sqlite3 = require ('sqlite3').verbose();
+var db = new sqlite3.Database (data/database.db)
+db.run ("CREATE TABLE IF NOT EXISTS coders ("+
+    "id INTEGER PRIMARY KEY, " +
+    "profile_photo_filename TEXT ," +
+    "name TEXT NOT NULL", +
+    "email TEXT NOT NULL", +
+    "charity_name TEXT" +
+    ");");
+
+db.run ("CREATE TABLE IF NOT EXISTS donors ("+
+    "id INTEGER PRIMARY KEY, " +
+    "profile_photo_filename TEXT ," +
+    "name TEXT NOT NULL", +
+    "email TEXT NOT NULL", +
+    "charity_name TEXT" +
+    "cents_pledged INTEGER NOT NULL"
+    ");");
+
+db.close();
